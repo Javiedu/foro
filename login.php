7383e725e5869480f6892981db9810035b32aa4e
Javiedu <jafezca.edu@gmail.com>
2022-01-21 20:59:10 +0100

    Inicio y creacion login

---
+++ login.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Signa</title>
+        <link rel="stylesheet" href="estilos/login.css">
+        <script src="https://kit.fontawesome.com/d065ecc10d.js" crossorigin="anonymous"></script>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <div class="main">
+            <div class="logo" ><i class="fas fa-signature"></i></div>
+            <form action="" method="post">
+                <p>Usuario</p>
+                <input class="campos" type="text" name="usuario" id="usuario">
+                <p>Contraseña</p>
+                <input class="campos" type="password" name="password" id="password">
+                <br>
+                <input class="boton" type="submit" name="validar" value="Iniciar Sesion">
+            </form>
+        </div>
+    </body>
+</html>

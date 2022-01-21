174ea92657c1aa590c48dfd814e2c6217aea34a8
Javiedu <jafezca.edu@gmail.com>
2022-01-21 21:51:55 +0100

    Barra lateral del panel

---
+++ paginas/panel.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <title>Panel</title>
+        <link rel="stylesheet" href="../estilos/panel.css">
+        <script src="https://kit.fontawesome.com/d065ecc10d.js" crossorigin="anonymous"></script>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <div class="lateral">
+            <div class="main_iconos">
+                <div class="logo"><a href="#"><i class="fas fa-signature"></i></a></div>
+                <div class="div_paginas"><a href="#"><i class="fas fa-clock paginas"></i></a></div>
+                <div class="div_paginas"><a href="#"><i class="fas fa-fire paginas"></i></a></div>
+                <div class="div_paginas"><a href="#"><i class="fas fa-folder paginas"></i></a></div>
+            </div>
+            <div class="main_opciones">
+                <div class="caja_opciones">
+                    <div class="div_opciones"><a href="#"><i class="fas fa-cog opciones"></i></a></div>
+                    <div class="div_opciones"><a href="#"><i class="fas fa-sign-out-alt opciones"></i></a></div>
+                </div>
+            </div>
+        </div>
+        <div class="principal">
+            <div class="superior">
+
+            </div>
+            <div class="panel">
+                
+            </div>
+        </div>
+    </body>
+</html>

ac0abc9661c23301fa32ef43d5b2334b753262b9
Javiedu <jafezca.edu@gmail.com>
2022-01-22 13:35:16 +0100

    Inicio entrada.php

---
+++ paginas/entrada.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <title>Panel</title>
+        <link rel="stylesheet" href="../estilos/lateral.css">
+        <link rel="stylesheet" href="../estilos/entrada.css">
+        <script src="https://kit.fontawesome.com/d065ecc10d.js" crossorigin="anonymous"></script>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@700;900&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <div class="lateral">
+            <div class="main_iconos">
+                <div class="logo"><a href="#"><i class="fas fa-signature"></i></a></div>
+                <div class="div_paginas"><a href="#"><i class="fas fa-clock paginas"></i></a></div>
+                <div class="div_paginas"><a href="#"><i class="fas fa-fire paginas"></i></a></div>
+                <div class="div_paginas"><a href="#"><i class="fas fa-folder paginas"></i></a></div>
+            </div>
+            <div class="main_opciones">
+                <div class="caja_opciones">
+                    <div class="div_opciones"><a href="#"><i class="fas fa-cog opciones"></i></a></div>
+                    <div class="div_opciones"><a href="#"><i class="fas fa-sign-out-alt opciones"></i></a></div>
+                </div>
+            </div>
+        </div>
+        <div class="principal">
+            <div class="superior">
+                <div class="perfil">
+                    <p>Maria Cruz Castells</p>
+                    <img src="../imagenes/perfil.png">
+                </div>
+            </div>
+            <div class="panel">
+                <div class="panel_interior">
+                </div>
+            </div>
+        </div>
+    </body>
+</html> 
